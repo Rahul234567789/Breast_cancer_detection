a3df16015164b3baac3b7ac4fe93c798127f7eb9
Rahul kumar <141143094+Rahul234567789@users.noreply.github.com>
2025-06-06 10:49:55 +0530

    Add files via upload
    This project focuses on developing a web-based machine learning application for early breast cancer detection. Users input clinical parameters through a web interface, and the system predicts the likelihood of breast cancer in percentage form. The backend uses supervised learning algorithms including Random Forest, Support Vector Machine (SVM), and Artificial Neural Networks to classify whether a tumor is benign or malignant. The model was trained on the Breast Cancer Wisconsin Dataset, with preprocessing steps like normalization, handling missing values, and feature selection to ensure high model performance.
    
    Key features include:
    
    User-friendly web interface for data input and real-time prediction
    
    Model evaluation using accuracy, precision, recall, and F1-score
    
    Comparative analysis of multiple ML algorithms to select the best-performing model
    
    Integration of the trained model into a web application using Flask (or other web frameworks if used)

---
+++ Major project/Major/Breast-Cancer-Detection-Using-Machine-Learning/app.py
@@ -0,0 +1,40 @@
+import numpy as np
+import pandas as pd
+from flask import Flask, request, render_template
+import pickle
+
+app = Flask(__name__)
+import os
+model_path = os.path.join(os.path.dirname(__file__), 'model.pkl')
+model = pickle.load(open(model_path, 'rb'))
+
+
+@app.route('/')
+def home():
+  return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+  input_features = [int(x) for x in request.form.values()]
+  features_value = [np.array(input_features)]
+
+  features_name = ['clump_thickness', 'uniform_cell_size', 'uniform_cell_shape',
+       'marginal_adhesion', 'single_epithelial_size', 'bare_nuclei',
+       'bland_chromatin', 'normal_nucleoli', 'mitoses']
+
+  df = pd.DataFrame(features_value, columns=features_name)
+  output = model.predict(df)
+
+  if output == 4:
+        return render_template('yes.html')
+
+  else:
+        return render_template('no.html')
+
+    
+    
+
+if __name__ == "__main__":
+  app.run()
+
+
